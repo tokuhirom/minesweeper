19d60a9cc94860d63bbeef49513c608d414e4af8
Tokuhiro Matsuno <tokuhirom@gmail.com>
2020-05-14 15:03:52 +0900

    snapshot

--- minesweeper.js
+++ minesweeper.js
@@ -1,7 +1,45 @@
 class App {
-    init() {
+    constructor() {
         console.log("hello")
-        this.board = new Board(10, 8);
+        this.width = 10
+        this.height = 8
+        this.board = new Board(this.width, this.height);
+        this.boardElement = document.getElementById('board')
+        this.isBombed = false
+    }
+
+    init() {
+        // TODO: 最初の一個は爆発しない
+        console.log(this.board.matrix.dumpInternal(true))
+        this.render()
+    }
+
+    render() {
+        // clear
+        this.boardElement.innerHTML = ''
+
+        // rendering!
+        for (let y=0; y<this.height; y++) {
+            for (let x=0; x<this.height; x++) {
+                const b = document.createElement('button')
+                b.addEventListener('click', () => {
+                    console.log(`Opening: x=${x} y=${y}`)
+                    if (this.board.hasBomb(x, y)) {
+                        // TODO: かおをかえる
+                        alert("bomb!")
+                        this.isBombed = true
+                        this.render()
+                    } else {
+                        this.board.openCell(x, y)
+                    }
+                    this.render()
+                });
+                b.innerText = this.board.getCellText(x, y, this.isBombed)
+                this.boardElement.append(b)
+            }
+            const br = document.createElement('br')
+            this.boardElement.append(br)
+        }
     }
 }
 
@@ -13,16 +51,19 @@ class Board {
     constructor(width, height) {
         this.width = width;
         this.height = height;
-        this.matrix = new Matrix(() => new Cell());
+        this.matrix = new Matrix(width, height, () => new Cell(0.1));
     }
 
-    open(x, y) {
-        if (this.matrix.hasBomb(x, y)) {
-            // TODO: かおをかえる
-            alert("bomb!")
-        } else {
-            this.matrix.openCell(x, y)
-        }
+    hasBomb(x, y) {
+        return this.matrix.hasBomb(x, y)
+    }
+
+    openCell(x, y) {
+        this.matrix.openCell(x, y)
+    }
+
+    getCellText(x, y, forceOpened) {
+        return this.matrix.getCell(x, y).getText(forceOpened)
     }
 }
 
@@ -31,8 +72,24 @@ class Cell {
     isOpened;
 
     constructor(bombRate) {
-        this.hasBomb = Math.random() > bombRate
+        if (typeof bombRate === 'undefined') {
+            throw 'bonbRate is required.'
+        }
+        this.hasBomb = Math.random() <= bombRate
         this.isOpened = false
+    }
+
+    getText(forceOpened) {
+        if (forceOpened || this.isOpened) {
+            if (this.hasBomb) {
+                return '*'
+            } else {
+                // TODO: 爆弾が周囲にあるセルには数字を出す
+                return '_'
+            }
+        } else {
+            return 'X'
+        }
     }
 }
 
@@ -45,10 +102,10 @@ class Matrix {
         this.height = height;
 
         this.cells = new Array(this.height)
-        for (let y=0; y<this.width; y++) {
+        for (let y=0; y<this.height; y++) {
             this.cells[y] = new Array(this.height);
             for (let x=0; x<this.width; x++) {
-                this.cells[y][x] = initializer();
+                this.cells[y][x] = initializer(this);
             }
         }
     }
@@ -68,12 +125,22 @@ class Matrix {
         return this.cells[y][x]
     }
 
+    dumpInternal(forceOpened) {
+        let buf = ''
+        for (let y=0; y<this.height; y++) {
+            for (let x=0; x<this.width; x++) {
+                buf += this.cells[y][x].getText(forceOpened) + ' '
+            }
+            buf += "\n"
+        }
+        return buf
+    }
+
     openCell(x, y) {
         const target = this.getCell(x, y)
-        target.hasBomb = true
+        target.isOpened = true
 
         // TODO: まわりがオープンされているセルを、open 状態にする。
-        // TODO: 周囲セルに数字を出す
     }
 }
 
